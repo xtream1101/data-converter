44a4b0381a4277a452b2e02950d575b528159557
Richard Stoeffel <richard.stoeffel@l2inc.com>
2018-02-22 15:32:04 -0500

    NF: Add xlsx reading functionality

---
+++ data_converter/xlsx_helper.py
@@ -0,0 +1,56 @@
+from openpyxl import load_workbook
+import utils
+
+
+
+def _list_from_xlsx_with_header(wb):
+    sheet_data = []
+
+    for sheet in wb:
+        headers = [cell.value for cell in sheet.iter_rows(min_row=1, max_row=1).next()]
+        sheet_width = len(headers)
+        sheet_iterator = sheet.iter_rows(min_row=2)
+        # iterate through each sheet_iteartor, and append the headers and row value of index i, from 0 to
+        # to width of the sheet (number of columns)
+        sheet_data.append({sheet.title: [
+                { headers[i]: row[i].value for i in range(sheet_width) }
+                for row in sheet_iterator
+            ]
+        })
+
+    return sheet_data
+
+def _list_from_xlsx_without_header(wb):
+    sheet_data = []
+
+    for sheet in wb:
+        sheet_iterator = sheet.iter_rows(min_row=1)
+        # iterate through each sheet_iteartor, and append the headers and row value of index i, from 0 to
+        # to width of the sheet (number of columns)
+        sheet_data.append({sheet.title: [
+                    [ cell.value for cell in row ]
+                    for row in sheet_iterator
+                ]
+            })
+
+    return sheet_data
+
+
+def read_file(input_file, header=True):
+    # data_only returns values in xlsx files rather than formulae
+    ext = utils._get_file_ext(input_file)
+    if ext != 'xlsx':
+        raise ValueError("Expecting a xlsx file, but got: {ext}".format(ext=ext))
+
+    wb = load_workbook(filename=input_file, data_only=True)
+    sheet_data = _list_from_xlsx_with_header(wb) if header else _list_from_xlsx_without_header(wb)
+
+    return sheet_data
+
+def write_file():
+    pass
+
+
+if __name__ == "__main__":
+    print(read_file("/Users/richard/Desktop/test.xlsx"))
+    print(read_file("/Users/richard/Desktop/test.xlsx", header=False))
---
+++ tests/xlsx/xlsx_test.py
@@ -0,0 +1,32 @@
+import datetime
+
+OUTPUT_LIST = [{u'Sheet1': [
+    {u'holla': u'jedi', u'bomb': 1L, u'price': 12L, u'the': 124L, u'com': datetime.datetime(2018, 2, 20, 0, 0),
+     u'is': u'super rad', u'dot': False},
+    {u'holla': u'sith', u'bomb': 4L, u'price': 25L, u'the': 54L, u'com': datetime.datetime(2018, 2, 20, 0, 0),
+     u'is': u'hella cool', u'dot': True},
+    {u'holla': u'jedi', u'bomb': None, u'price': 55L, u'the': 45L, u'com': datetime.datetime(2018, 2, 20, 0, 0),
+     u'is': u'fly as shit', u'dot': False},
+    {u'holla': u'sith', u'bomb': None, u'price': 23L, u'the': 4L, u'com': datetime.datetime(2018, 2, 20, 0, 0),
+     u'is': u'dope to the max', u'dot': True},
+    {u'holla': u'jei', u'bomb': 1L, u'price': 51053L, u'the': 154L, u'com': datetime.datetime(2018, 2, 20, 0, 0),
+     u'is': u'wavy extream', u'dot': False}]},
+    {u"Price's super cool second sheet": [
+        {u'of ': True, u'glory': datetime.datetime(2018, 2, 20, 0, 0), u'blades': 47L},
+        {u'of ': False, u'glory': datetime.datetime(2018, 2, 20, 0, 0), u'blades': 12L},
+        {u'of ': True, u'glory': datetime.datetime(2018, 2, 20, 0, 0), u'blades': 18L}]}]
+SHEET1 = {u'Sheet1':
+                  [{u'holla': u'jedi', u'bomb': 1L, u'price': 12L, u'the': 124L,
+                    u'com': datetime.datetime(2018, 2, 20, 0, 0), u'is': u'super rad', u'dot': False},
+                   {u'holla': u'sith', u'bomb': 4L, u'price': 25L, u'the': 54L,
+                    u'com': datetime.datetime(2018, 2, 20, 0, 0), u'is': u'hella cool', u'dot': True},
+                   {u'holla': u'jedi', u'bomb': None, u'price': 55L, u'the': 45L,
+                    u'com': datetime.datetime(2018, 2, 20, 0, 0), u'is': u'fly as shit', u'dot': False},
+                   {u'holla': u'sith', u'bomb': None, u'price': 23L, u'the': 4L,
+                    u'com': datetime.datetime(2018, 2, 20, 0, 0), u'is': u'dope to the max', u'dot': True},
+                   {u'holla': u'jei', u'bomb': 1L, u'price': 51053L, u'the': 154L,
+                    u'com': datetime.datetime(2018, 2, 20, 0, 0), u'is': u'wavy extream', u'dot': False}]}
+SHEET2= {u"Price's super cool second sheet":
+                 [{u'of ': True, u'glory': datetime.datetime(2018, 2, 20, 0, 0), u'blades': 47L},
+                  {u'of ': False, u'glory': datetime.datetime(2018, 2, 20, 0, 0), u'blades': 12L},
+                  {u'of ': True, u'glory': datetime.datetime(2018, 2, 20, 0, 0), u'blades': 18L}]}
